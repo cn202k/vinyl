866efd7a9ba4f53f051fb2c80dff1ceef3e346d7
cn202k <cn202k@gmail.com>
2021-03-29 17:25:28 +0900

    changed the filename

--- vinyl/lib/src/copy.dart
+++ vinyl/lib/src/copy.dart
@@ -1,3 +1,3 @@
-import 'package:vinyl/src/builder.dart';
+import 'package:vinyl/src/data_builder.dart';
 
 T copy<T>(DataBuilder<T> builder) => builder.build();
--- vinyl/lib/vinyl.dart
+++ vinyl/lib/vinyl.dart
@@ -2,5 +2,5 @@ library vinyl;
 
 export 'package:collection/collection.dart';
 export 'package:vinyl/src/annotations.dart';
-export 'package:vinyl/src/builder.dart';
+export 'package:vinyl/src/data_builder.dart';
 export 'package:vinyl/src/copy.dart';
